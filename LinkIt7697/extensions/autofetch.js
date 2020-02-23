a6f09a9f84b053ef36b1d4f835ccb33401252068
lioujj <34760803+lioujj@users.noreply.github.com>
2020-02-23 23:02:51 +0800

    Update autofetch.js

--- LinkIt7697/extensions/autofetch.js
+++ LinkIt7697/extensions/autofetch.js
@@ -16,9 +16,9 @@
 				setCategoryName("category_liou", Blockly.Msg.CATEGORY_LIOU);
 			}
 			setTimeout(function(){
-				//setCategoryName("category_liou", Blockly.Msg.CATEGORY_LIOU);
+				setCategoryName("category_liou", Blockly.Msg.CATEGORY_LIOU);
 				// Blockly.updateToolbox(toolbox);  //目前會造成目錄大風吹，可考慮不執行這行程式碼，目錄僅顯示toolbox.xml內設定的名稱
-			}, 1000);
+			}, 3000);
 		}
 	}, 100); 
 
