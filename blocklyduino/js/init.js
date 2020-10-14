73ccbf2c1208af775eb08759a3f67bd29afa3ec7
lioujj <34760803+lioujj@users.noreply.github.com>
2020-10-14 10:48:37 +0800

    Add files via upload

---
+++ blocklyduino/js/init.js
@@ -0,0 +1,481 @@
+/**
+ * List of tab names.
+ * @private
+ */
+
+'use strict';
+
+var TABS_ = ['blocks', 'arduino'];
+
+var selected = 'blocks';
+
+var chosenEntry = null;
+
+var current_lang = "";
+
+/**
+ * Switch the visible pane when a tab is clicked.
+ * @param {string} clickedName Name of tab clicked.
+ */
+function tabClick(clickedName) {
+  // Deselect all tabs and hide all panes.
+  for (var i = 0; i < TABS_.length; i++) {
+    var name = TABS_[i];
+    document.getElementById('tab_' + name).className = 'taboff';
+    document.getElementById('content_' + name).style.visibility = 'hidden';
+  }
+
+  // Select the active tab.
+  selected = clickedName;
+  document.getElementById('tab_' + clickedName).className = 'tabon';
+  // Show the selected pane.
+  document.getElementById('content_' + clickedName).style.visibility = 'visible';
+  renderContent();
+  Blockly.fireUiEvent(window, 'resize');
+}
+
+/**
+ * Populate the currently selected pane with content generated from the blocks.
+ */
+function renderContent() {
+  var content = document.getElementById('content_' + selected);
+  // Initialize the pane.
+  if (content.id == 'content_blocks') {
+    // If the workspace was changed by the XML tab, Firefox will have performed
+    // an incomplete rendering due to Blockly being invisible.  Rerender.
+    Blockly.mainWorkspace.render();
+  } else if (content.id == 'content_arduino') {
+    //content.innerHTML = Blockly.Arduino.workspaceToCode();
+    var arduinoTextarea = document.getElementById('content_arduino');
+    arduinoTextarea.value = Blockly.Arduino.workspaceToCode();
+    arduinoTextarea.focus();
+  }
+}
+
+/**
+ * Compute the absolute coordinates and dimensions of an HTML element.
+ * @param {!Element} element Element to match.
+ * @return {!Object} Contains height, width, x, and y properties.
+ * @private
+ */
+function getBBox_(element) {
+  var height = element.offsetHeight;
+  var width = element.offsetWidth;
+  var x = 0;
+  var y = 0;
+  do {
+    x += element.offsetLeft;
+    y += element.offsetTop;
+    element = element.offsetParent;
+  } while (element);
+  return {
+    height: height,
+    width: width,
+    x: x,
+    y: y
+  };
+}
+
+/**
+ * Initialize Blockly.  Called on page load.
+ */
+function buildBlocks(xmlValue) {
+  //window.onbeforeunload = function() {
+  //  return 'Leaving this page will result in the loss of your work.';
+  //};
+  var container = document.getElementById('content_area');
+  var onresize = function (e) {
+    var bBox = getBBox_(container);
+    for (var i = 0; i < TABS_.length; i++) {
+      var el = document.getElementById('content_' + TABS_[i]);
+      el.style.top = bBox.y + 'px';
+      el.style.left = bBox.x + 'px';
+      // Height and width need to be set, read back, then set again to
+      // compensate for scrollbars.
+      el.style.height = bBox.height + 'px';
+      el.style.height = (2 * bBox.height - el.offsetHeight) + 'px';
+      el.style.width = bBox.width + 'px';
+      el.style.width = (2 * bBox.width - el.offsetWidth) + 'px';
+    }
+    // Make the 'Blocks' tab line up with the toolbox.
+    if (Blockly.mainWorkspace.toolbox_.width) {
+      document.getElementById('tab_blocks').style.minWidth =
+        (Blockly.mainWorkspace.toolbox_.width - 38) + 'px';
+      // Account for the 19 pixel margin and on each side.
+    }
+  };
+  window.addEventListener('resize', onresize, false);
+
+//----------------------------------------------------
+
+//var preHTTP="https://lioujj.github.io/LinkIt7697/extensions_0922/"
+var preHTTP="extensions_0922/";
+var blocks_path = preHTTP+"blocks.js";   //載入自訂積木定義檔	
+var javascript_path = preHTTP+"javascript.js";   //載入自訂積木轉出程式碼檔	
+var toolbox_path = preHTTP+"toolbox.xml";  //載入自訂積木目錄檔	
+var en_path = preHTTP+"en.js";  //載入積木文字英文語系設定檔	
+var en_category_path = preHTTP+"en_category.xml";  //載入積木目錄文字英文語系設定檔
+var zhhant_path = preHTTP+"zh-hant.js";  //載入積木文字繁體語系設定檔(預設繁體語系)
+var zhhant_category_path = preHTTP+"zh-hant_category.xml";  //載入積木目錄文字繁體語系設定檔(預設繁體語系)
+var insertAfterCategoryName = "category_linkit_ble_ibeacon";  //可將自訂積木插入在指定目錄後
+
+initial(blocks_path, javascript_path, toolbox_path, insertAfterCategoryName);
+
+function initial(blocks_path, javascript_path, toolbox_path) {
+	addScript(blocks_path);
+	addScript(javascript_path);
+	var xml = $.ajax({url: toolbox_path, async: false}).responseXML;
+
+	var len = new DOMParser().parseFromString(xmlValue,"text/xml").firstChild.childNodes.length;
+	var xmlNewValue='<xml id="toolbox">';
+	if (len>0) {
+		for (var i=0;i<len;i++){
+			var node = new XMLSerializer().serializeToString(new DOMParser().parseFromString(xmlValue,"text/xml").firstChild.childNodes[i]);
+			xmlNewValue+=node;
+			if (node.indexOf(insertAfterCategoryName)!=-1)
+				xmlNewValue+='<sep></sep>'+new XMLSerializer().serializeToString(xml.firstChild);
+		}
+	}
+	xmlNewValue+='</xml>';
+	xmlValue = xmlNewValue;
+	
+	if ((document.getElementById('select-lang-en').checked)) {
+		addScript(en_path);
+		var xml = $.ajax({url: en_category_path, async: false}).responseXML.firstChild;
+	} else {
+		addScript(zhhant_path);
+		var xml = $.ajax({url: zhhant_category_path, async: false}).responseXML.firstChild;
+	}
+	xml = new DOMParser().parseFromString(new XMLSerializer().serializeToString(xml).replace(/\n/g, "").replace(/\r/g, "").replace(/\t/g, ""),"text/xml").firstChild;
+	
+	for (var i=0;i<xml.childNodes.length;i++) {
+		if (xml.childNodes[i].nodeName.toLowerCase()=="category") {
+			var ini = xml.childNodes[i].childNodes[0].firstChild.nodeValue;
+			var rep = xml.childNodes[i].childNodes[1].firstChild.nodeValue;
+			xmlValue = xmlValue.replace('name="'+ini+'"','name="'+rep+'"');	
+		}
+	}
+}
+
+function addScript(url) {
+	var s = document.createElement("script");
+	s.type = "text/javascript";
+	s.src = url;
+	$("body").append(s);
+}
+//-------------------------
+
+
+
+
+  Blockly.inject(document.getElementById('content_blocks'),{
+    grid:
+    {spacing: 25,
+     length: 3,
+     colour: '#ccc',
+     snap: true},
+    //media: 'media/',
+    media: filepath.media,
+    toolbox: xmlValue});
+
+  //auto_save_and_restore_blocks();
+  setCheckbox();
+
+  //load from url parameter (single param)
+  //http://stackoverflow.com/questions/2090551/parse-query-string-in-javascript
+  var dest = unescape(location.search.replace(/^.*\=/, '')).replace(/\+/g, " ");
+  if (dest) {
+    //load_by_url(dest);
+  }
+}
+
+function versionCompare(v1, v2, options) {
+    var lexicographical = options && options.lexicographical,
+        zeroExtend = options && options.zeroExtend,
+        v1parts = v1.split('.'),
+        v2parts = v2.split('.');
+
+    function isValidPart(x) {
+        return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+    }
+
+    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+        return NaN;
+    }
+
+    if (zeroExtend) {
+        while (v1parts.length < v2parts.length) v1parts.push("0");
+        while (v2parts.length < v1parts.length) v2parts.push("0");
+    }
+
+    if (!lexicographical) {
+        v1parts = v1parts.map(Number);
+        v2parts = v2parts.map(Number);
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v2parts.length == i) {
+            return 1;
+        }
+
+        if (v1parts[i] == v2parts[i]) {
+            continue;
+        }
+        else if (v1parts[i] > v2parts[i]) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    if (v1parts.length != v2parts.length) {
+        return -1;
+    }
+
+    return 0;
+}
+
+function init() {
+  var loadIds;
+var base = "category_logic,category_loops,category_array,category_math,category_text,category_variables,category_functions,category_sep,category_initializes,category_digital,category_analog,category_serial,category_others,category_time,category_interrupts,category_servo,category_sep,category_linkit_wifi,category_linkit_mcs,category_linkit_lremote,category_linkit_ble,category_linkit_ble_ibeacon,category_sep,category_mooncar,category_ez_start_kit,category_sep,category_sensor,category_display,category_transceiver,category_sep,category_grove,category_sep,category_external";
+
+  try {
+    var manifestData = chrome.runtime.getManifest();
+    var updateURL = manifestData.update_url;
+    var isPreRelease = false;
+    if (manifestData.version_name.indexOf('b', manifestData.version_name.length - 1) !== -1) {
+        isPreRelease = true;
+        updateURL = updateURL.replace('/master/', '/dev/');
+        base += ",category_sep,category_beta";
+        Materialize.toast(Blockly.Msg.ERROR_BETA_WARNING, 10000);
+    }
+
+    if (manifestData.version.length > 0) {
+      var client = new XMLHttpRequest();
+      client.open('GET', updateURL);
+      client.onreadystatechange = function() {
+        if (client.readyState == 4 && client.status == 200) {
+          if (client.responseText) {
+             var resp = JSON.parse(client.responseText);
+             if (versionCompare(resp.version, manifestData.version) > 0) {
+               var message = Blockly.Msg.MESSAGE_UPDATE + resp.version;
+               if (isPreRelease) {
+                 message += Blockly.Msg.SETTINGS_VERSION_PRE_RELEASE;
+               }
+               message += Blockly.Msg.MESSAGE_UPDATE_APPEND;
+               Materialize.toast(message, 10000);
+             }
+          }
+        }
+      }
+      client.send();
+    }
+  }
+  catch(err) {
+
+  }
+  finally {
+
+  }
+
+  chrome.storage.local.get('toolboxids', function (value) {
+    var option = value.toolboxids;
+    // set the default toolbox if none
+    if (option === undefined || option === "") {
+      loadIds = base;
+    }else{
+      loadIds = base + ',' + option;
+    }
+    var xmlValue = '<xml id="toolbox">';
+    var xmlids = loadIds.split(",");
+    for (var i = 0; i < xmlids.length; i++) {
+      if ($('#'+xmlids[i]).length) {
+        xmlValue += $('#'+xmlids[i])[0].outerHTML;
+      }
+    }
+    xmlValue += '</xml>';
+    buildBlocks(xmlValue);
+    tabClick('blocks');
+  })
+};
+
+function setCheckbox(){
+  var option;
+
+  chrome.storage.local.get("toolboxids", function (value) {
+    option = value.toolboxids;
+    if (option === undefined || option === "") {
+      return;
+    }
+    var options = option.split(",");
+    for (var i = 0; i < options.length; i++) {
+      $('#chbox_' + options[i]).prop("checked",true);
+    }
+  })
+}
+
+function setScript(param) {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.id = 'msg';
+  script.src = filepath["msg_"+param];
+  var str = "#select-lang-"+ param;
+  $(str).prop('checked', true);
+  current_lang = param;
+
+  var firstScript = document.getElementsByTagName('head')[0].appendChild(script);
+  firstScript.parentNode.insertBefore(script, firstScript);
+  script.onload = function (e) {
+    setCharacter();
+    init();
+  }
+}
+
+function setCharacter(){
+  setCategoryCharacter();
+
+  try {
+    var manifestData = chrome.runtime.getManifest();
+    $("#version").on('click', function(){
+        nw.Shell.openExternal(manifestData.update_url);
+        return false;
+    });
+    if (manifestData.version_name.indexOf('b', manifestData.version_name.length - 1) !== -1) {
+        $("#version").text(Blockly.Msg.SETTINGS_VERSION + manifestData.version + Blockly.Msg.SETTINGS_VERSION_PRE_RELEASE);
+    } else {
+        $("#version").text(Blockly.Msg.SETTINGS_VERSION + manifestData.version);
+    }
+  }
+  catch(err) {
+    $("#version").remove();
+  }
+  finally {
+
+  }
+
+  $("#tab_blocks").text(Blockly.Msg.BLOCKS);
+  $("#tab_arduino").text(Blockly.Msg.ARDUINO);
+
+  $("#go-to-web").attr("data-tooltip",Blockly.Msg.GO_TO_WEB);
+  $("#go-to-sample").attr("data-tooltip",Blockly.Msg.GO_TO_SAMPLE);
+  $("#open-setting").attr("data-tooltip",Blockly.Msg.SETTING);
+  $("#dialog-lang-title").text(Blockly.Msg.DIALOG_LANG_TITLE);
+  $("#dialog-block-title").text(Blockly.Msg.DIALOG_BLOCK_TITLE);
+
+  $("#auto-save-title").text(Blockly.Msg.AUTO_SAVE_TITLE);
+  $("#range-title").html(Blockly.Msg.RANGE_TITLE + '<input type="range" id="save-time" min="1" max="10" />');
+
+  $("#setting-cancel").text(Blockly.Msg.SETTINGS_CANCEL);
+  $("#setting").text(Blockly.Msg.SETTINGS_OK);
+
+  $("#button_new").attr("data-tooltip",Blockly.Msg.BUTTON_NEW);
+  $("#button_save").attr("data-tooltip",Blockly.Msg.BUTTON_SAVE);
+  $("#button_open").attr("data-tooltip",Blockly.Msg.BUTTON_OPEN);
+  //$("#button_discard").text(Blockly.Msg.DROPDOWN_DISCARD);
+  $("#button_save_as").text(Blockly.Msg.DROPDOWN_SAVE_AS);
+  $("#dialog0_title").text(Blockly.Msg.DIALOG0_TITLE);
+  $("#dialog1_title").text(Blockly.Msg.DIALOG1_TITLE);
+  $("#dialog1_yes").text(Blockly.Msg.DIALOG1_YES);
+  $("#dialog1_no").text(Blockly.Msg.DIALOG1_NO);
+  $("#dialog4_title").text(Blockly.Msg.DIALOG4_TITLE);
+  $("#dialog4_yes").text(Blockly.Msg.DIALOG1_YES);
+  $("#dialog4_no").text(Blockly.Msg.DIALOG1_NO);
+  $("#info_filename").html(Blockly.Msg.INFO_FILENAME);
+  $("#info_title").html(Blockly.Msg.INFO_TITLE);
+  $("#dialog2_title").text(Blockly.Msg.DIALOG2_TITLE);
+
+  $("#button_import").text(Blockly.Msg.BUTTON_IMPORT);
+  $("#button_export").text(Blockly.Msg.BUTTON_EXPORT);
+  $('#textarea_import_label').text(Blockly.Msg.TEXTAREA_IMPORT_LABEL);
+  $('#textarea_export_label').text(Blockly.Msg.TEXTAREA_EXPORT_LABEL);
+  $('#dialog_import_ok').text(Blockly.Msg.DIALOG_IMPORT_OK);
+  $('#dialog_import_cancel').text(Blockly.Msg.DIALOG_IMPORT_CANCEL);
+  $('#dialog_export_ok').text(Blockly.Msg.DIALOG_EXPORT_OK);
+
+  $("#button_launch_ide").attr("data-tooltip",Blockly.Msg.BUTTON_LAUNCH_IDE);
+  $("#button_upload").attr("data-tooltip",Blockly.Msg.BUTTON_UPLOAD);
+  $("#button_board_settings").attr("data-tooltip",Blockly.Msg.BUTTON_BOARD_SETTINGS);
+  $("#modal_text_board_settings").text(Blockly.Msg.BUTTON_BOARD_SETTINGS);
+  $("#text_uploader_status").text(Blockly.Msg.TEXT_UPLOADER_STATUS);
+  $("#label_text_board").text(Blockly.Msg.TEXT_BOARD);
+  $("#label_text_com_port").text(Blockly.Msg.TEXT_COM_PORT);
+  $("#dropdown_item_scanning").text(Blockly.Msg.DROPDOWN_SCANNING);
+
+  // Load upload COM port setting from local storage
+  try {
+    chrome.storage.local.get(['COM'], function(item) {
+      if(item.COM) {
+        console.log("reload COM port setting to " + item.COM);
+        selectUploadPort(item.COM);
+      }
+    });
+  } catch (err) {
+    console.log(err)
+  }
+
+}
+
+function export_xml(){
+  var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+  var data = Blockly.Xml.domToText(xml);
+  data = data.replace(/\r?\n/g,'');
+  $('#textarea_export').val(data);
+  $('#textarea_export').trigger('autoresize');
+  $('#modal_export').openModal();
+}
+
+function import_xml(){
+  var xml = $('#textarea_import').val();
+  $('#textarea_import').val("");
+  var xmlDoc = Blockly.Xml.textToDom(xml);
+  // Blockly.mainWorkspace.clear();
+  Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xmlDoc);
+}
+
+window.onload = function () {
+  var keys = [ 'lang' ];
+  // load from localStorage
+  chrome.storage.local.get(keys, function(item){
+    if(!item.lang){
+      var item = {
+        'lang': 'zh'
+      };
+      // save to localStorage
+      chrome.storage.local.set(item, function(){
+        console.log('item saved.');
+      });
+      setScript("zh");
+    }else{
+      setScript(item.lang);
+    }
+  });
+};
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.method == "url"){
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET",request.url,true);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          // WARNING! Might be evaluating an evil script!
+          newFile();
+          Blockly.mainWorkspace.clear();
+          var xml = xhr.responseText;
+          xml = xml.replace("<html><head/><body><xml>",'');
+          xml = xml.replace("</body></html>",'');
+          var xmlDoc = Blockly.Xml.textToDom(xml);
+          Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xmlDoc);
+        }
+      }
+      xhr.send();
+      sendResponse({farewell: "goodbye"});
+    }else if(request.method == "autosave"){
+      if(Entryflg != 0 && hasWriteAccess){
+        handleSaveButton();
+        //Materialize.toast(Blockly.Msg.POPUP_SAVE_DONE, 4000) // 4000 is the duration of the toast
+      }
+      sendResponse({farewell: "goodbye"});
+    }
+});
---
+++ blocklyduino/js/upload.js
@@ -0,0 +1,273 @@
+'user strict'
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#button_upload')
+      .addEventListener('click', handleUploadButton);
+  document.querySelector('#button_launch_ide')
+      .addEventListener('click', openArduinoIDE);
+  document.querySelector('#button_launch_serial')
+      .addEventListener('click', openSerialMonitor);
+
+});
+
+var nwGui = require('nw.gui'), nwShell = nwGui.Shell,
+    child_process = require('child_process'), exec = child_process.exec,
+    execSync = child_process.execSync, execFile = child_process.execFile,
+    execFileSync = child_process.execFileSync, spawn = child_process.spawn;
+var iconv = require('iconv-lite');
+// var fs = require('fs');
+var fs = require('fs-extra');
+var path = require('path');
+
+var availPorts = [], nextAvailPorts = [];
+var selectedPort = null;
+var cmd_encoding = 'Big5';
+var process;
+
+var tmpInoDir = 'sketches/tmp/';
+var tmpInoFilename = 'tmp.ino';
+var tmpBuildDir = 'build/tmp/';
+
+function closeSerialMonitor() {
+  return new Promise(function(resolve, reject) {
+    process = exec('taskkill /F /IM putty.exe', {encoding: 'buffer'});
+    // wait for taskkill to terminate
+    process.on('exit', resolve);
+  });
+}
+
+function handleUploadButton() {
+  closeSerialMonitor().then(function() {
+    fs.ensureDirSync(tmpBuildDir);
+    writeInoFile(tmpInoDir, tmpInoFilename);
+    startUploading(tmpInoDir + tmpInoFilename);
+  });
+}
+
+function openArduinoIDE() {
+  // check if user has saved the INO file
+  if (!fileEntry) {
+    Materialize.toast(
+        Blockly.Msg.SAVE_FIRST, 4000)  // 4000 is the duration of the toast
+    return
+  }
+
+  // Open the INO file with Arduino.exe
+  closeSerialMonitor().then(function() {
+    // the "fileEntry" is actually a directly entry.
+    // so we search the INO file name within the directory entry first
+    var filename = document.getElementById('info_title').innerHTML;
+    var dirname = filename.split('.')[0]
+    var searchname = filename
+    if(Entryflg==2){
+      searchname = dirname + '/' + filename
+    }
+    fileEntry.getFile(searchname, {create: false}, function(inoEntry) {
+      // The inoEntry is a chrome-specific file path
+      // we need to convert it to the "display path",
+      // which is UNC Path under Windows,
+      // before passing the path to Arduino.exe
+      chrome.fileSystem.getDisplayPath(inoEntry, function(inoPath) {
+        console.log('arduino.exe ' + inoPath);
+        let command = '"arduino-1.8.5\\arduino.exe"'
+        let parameter = inoPath;
+        var process = exec(command + ' ' + parameter, {encoding: 'buffer'});
+      });
+    }, errorHandler);
+  });
+}
+
+function getSerialBaudRate() {
+  // This function scans for "serial.begin" blocks and determines the 
+  // baudrate of the sketch.
+  var baudrate = 9600;
+
+  // get the converted code
+  var code = Blockly.Arduino.workspaceToCode();
+  // scan for the Serial
+  var matches = code.match(/Serial\.begin\((\d+)\)/);
+  if(matches) {
+    // extract sub-string matches, if any.
+    baudrate = matches[1]
+  }
+  
+  return baudrate.toString()
+}
+
+function openSerialMonitor() {
+  closeSerialMonitor().then(function() {
+    var process =
+        execFile('putty.exe',  
+          [
+            '-serial', selectedPort,
+            '-sercfg', getSerialBaudRate() + ',8,n,1,N',
+          ]
+          , {encoding: 'buffer'});
+    console.log(process);
+  });
+}
+
+function outputUploaderMsg(message, className = null) {
+  let el_output = document.getElementById('terminal-body');
+  let node = document.createElement('DIV');
+  if (className) {
+    node.classList.add(className);
+  }
+  let textnode = document.createTextNode(message);
+  node.appendChild(textnode);
+  el_output.appendChild(node);
+  el_output.scrollTop = el_output.scrollHeight;
+}
+
+function clearUploaderMsg() {
+  $('#terminal-body').empty();
+}
+
+// by default, convert 'Big5' to 'utf8'
+function decode(buf, encoding = cmd_encoding) {
+  return iconv.decode(buf, encoding);
+}
+
+function startUploading(inoPath) {
+  uiStartUploading();
+  clearUploaderMsg()
+
+  let board = document.getElementById('board-selector').value;
+  let port = selectedPort;
+
+  outputUploaderMsg('Launching Arduino.exe...', 'msg-warning');
+  
+  // TODO:
+  // we use arduino_debug.exe because 
+  // it waits for the completion of child processes javaw and arduino_builder. 
+  // The non-debug version(arduon.exe) terminates immediately.
+  //
+  // Furthermore, we need to turn off the verbose-build mode,
+  // because arduino_debug.exe will "exit" early
+  // when there are excessive stdout messages printed.
+  // 
+  // This seems to be a bug in the node.js childprocess module.
+  // The child process (arduino_debug.exe) is actually
+  // alive and well, but node.js sends an `exit` event
+  // and stops piping stdout / stderr messages to
+  // our event callbacks.
+  process = execFile(
+    'arduino-1.8.5\\arduino_debug.exe', 
+    [
+      '--upload', inoPath,
+      '--board', board,
+      '--port', port,
+      '--pref', 'build.path=' + tmpBuildDir,
+      //'--verbose-build' --> verbose-build is disabled to keep arduino_debug.exe "alive".
+    ], {encoding: 'buffer'});
+
+  console.log("Arduino PID=", process.pid);
+  
+  process.stdout.on('data', function(data) {
+    let msg = decode(data);  
+    let re = /[\w+\.]+\.o|.+\.\.\.|Build\s.+all/;
+    //let re = /.+\.\.\./;
+    let matched = msg.match(re);
+    console.log(msg);
+    if(matched){
+      console.log(matched[0])
+      outputUploaderMsg(matched[0]);
+    }
+    
+  });
+
+  process.stderr.on('data', function(data) {
+    let msg = decode(data);
+    console.log(msg);
+    outputUploaderMsg(msg, 'msg-warning');
+  });
+
+  process.on('exit', function(code, signal) {
+    uiFinishUploading();
+    console.log('!!!!onExit of Arduino.exe!!!!');
+    outputUploaderMsg('Arduino.exe exited with ' + code + ' and ' + signal, 'msg-warning');
+  });
+}
+
+function uiStartUploading() {
+  clearInterval(detectTimer);
+
+  $('#button_upload').hide();
+  $('#uploading-spinner').addClass('active');
+}
+
+function uiFinishUploading() {
+  $('#uploading-spinner').removeClass('active');
+  $('#button_upload').show();
+  detectTimer = setInterval(detectPort, 5000);
+}
+
+/* port detect */
+
+function detectPort() {
+  let command = 'mode';
+
+  exec(command, {encoding: 'buffer'}, (error, stdout, stderr) => {
+    let output = decode(stdout);
+    nextAvailPorts = parsePorts(output);
+    updateAvailPorts();
+  })
+}
+
+function parsePorts(str) {
+  return str.match(/COM\d+/g) || [];
+}
+
+function updateAvailPorts() {
+  // check if availPorts equal to nextAvailPorts
+  if (availPorts.length == nextAvailPorts.length &&
+      availPorts.every((v, i) => v === nextAvailPorts[i])) {
+    return;
+  }
+  console.log('Available ports is changed. Update port-selector...');
+  console.log('nextAvailPorts:', nextAvailPorts);
+  availPorts = nextAvailPorts;
+  updatePortSelector(availPorts, selectedPort);
+}
+
+function selectUploadPort(port) {
+  selectedPort = port;
+
+  portLabel = document.querySelector('#port-selected-text')
+  portLabel.textContent = selectedPort + ' ';
+  portLabel.classList.toggle('blinking-text', false);
+
+  // Save upload COM port setting to local storage
+  chrome.storage.local.set({'COM': selectedPort}, function() {
+    console.log('store COM port setting to ' + selectedPort);
+  });
+
+  console.log('change upload port to ' + selectedPort);
+}
+
+function updatePortSelector(availPorts, selectedPort) {
+  let $dropdownPort = $('#dropdownPort');
+  $dropdownPort.empty();
+
+  if (availPorts.length) {
+    availPorts.forEach(port => {
+      // upload dropdown button's list
+      let item_str = '<li><a href="#!" onclick="selectUploadPort(\'' + port + '\')">' + port + '</a>'
+    item_str += '<li class="divider">'
+      $dropdownPort.append($(item_str))
+    });
+  } else {
+    // upload dropdown button's list
+    let item_str = '<li><a href="#!">' + Blockly.Msg.DROPDOWN_SCANNING + '</a>';
+    $dropdownPort.append($(item_str))
+  }
+}
+
+detectPort();
+detectTimer = setInterval(detectPort, 5000);
+
+function writeInoFile(dir, filename) {
+  fs.ensureDirSync(dir);
+  let code = Blockly.Arduino.workspaceToCode();
+  fs.writeFileSync(dir + filename, code);
+}
