8fce4429a2b4f472ba9b01a9ddc6e27d313689c3
lioujj <34760803+lioujj@users.noreply.github.com>
2022-03-15 13:15:41 +0800

    Add files via upload

--- LinkIt7697/extensions_0922/en.js
+++ LinkIt7697/extensions_0922/en.js
@@ -407,6 +407,7 @@ Blockly.Msg.BROADCAST_UDP_RECEIVED_MSG = "received message";
 Blockly.Msg.BROADCAST_UDP_RESET = "reset broadcast port:";
 Blockly.Msg.BROADCAST_UDP_WARNING = "You have to init your broadcast port first.";
 Blockly.Msg.BROADCAST_UDP_CHECK = "check UDP message";
+Blockly.Msg.BROADCAST_UDP_SEND_IP = "to IP:";
 
 //WEBSERVER
 Blockly.Msg.WEBSERVER_HELPURL= "https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697/網頁伺服器積木";
@@ -826,3 +827,15 @@ Blockly.Msg.PN532I2C_PAGE_List = [["4","4"],["5","5"],["6","6"],["7","7"],["8","
                                   ["15","15"],["16","16"],["17","17"],["18","18"],["19","19"],["20","20"],["21","21"],["22","22"],["23","23"],["24","24"],
                                   ["25","25"],["26","26"],["27","27"],["28","28"],["29","29"],["30","30"],["31","31"],["32","32"],["33","33"],["34","34"],
                                   ["35","35"],["36","36"],["37","37"],["38","38"],["39","39"]];
+
+//CCS811
+Blockly.Msg.CCS811_HELPURL = "";
+Blockly.Msg.CCS811_TITLE = "CCS811 AirQuality sensor";
+Blockly.Msg.CCS811_INIT = "init";
+Blockly.Msg.CCS811_TOOLTIP = "";
+Blockly.Msg.CCS811_UPDATE = "calculate the result";
+Blockly.Msg.CCS811_CHECK = "is dataAvailable?";
+Blockly.Msg.CCS811_WARNING= "You have to set \""+Blockly.Msg.CCS811_TITLE+" "+Blockly.Msg.CCS811_INIT+"\" first.";
+Blockly.Msg.CCS811_GET_DATA = "get the data of";
+Blockly.Msg.CCS811_FREQ= "set measurement frequence:every";
+Blockly.Msg.CCS811_FREQ_UNIT = "seconds";
