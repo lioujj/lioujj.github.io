39d8c08b1727b94aae072d70a299bc313ec4b35d
lioujj <34760803+lioujj@users.noreply.github.com>
2022-05-09 19:42:09 +0800

    Add files via upload

--- LinkIt7697/extensions/en.js
+++ LinkIt7697/extensions/en.js
@@ -867,6 +867,7 @@ Blockly.Msg.I2S_MIC_SAVE_TO = "record WAV file to";
 Blockly.Msg.I2S_MIC_REC_TIME = "time(secs)";
 Blockly.Msg.I2S_MIC_STT = "upload WAV file(Google) to recognize";
 Blockly.Msg.I2S_MIC_STT_AZURE = "upload WAV file(Azure) to recognize";
+Blockly.Msg.I2S_MIC_DIRECT_UPLOAD = "direct upload voice to Azure to recognize";
 Blockly.Msg.I2S_MIC_STT_RESULT = "get the recognition text";
 Blockly.Msg.I2S_MIC_UPLOAD_FROM = "from";
 Blockly.Msg.I2S_MIC_KEY = "API key";
