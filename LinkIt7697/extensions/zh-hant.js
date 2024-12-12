6c0db1fe601c048dfc7843a740889fa57ba8126d
lioujj <34760803+lioujj@users.noreply.github.com>
2024-12-12 12:21:26 +0800

    Add files via upload

--- LinkIt7697/extensions/zh-hant.js
+++ LinkIt7697/extensions/zh-hant.js
@@ -1388,7 +1388,7 @@ Blockly.Msg.LJJ_RADAR_03D_TARGET_LIST = [["目標1","0"],["目標2","1"],["目�
 Blockly.Msg.LJJ_RADAR_03D_TARGET_DATATYPE = [["x軸(mm)","x"],["y軸(mm)","y"],["移動速度(cm/s)","s"],["距離分辨率(mm)","d"],["角度","a"]];
 Blockly.Msg.LJJ_RADAR_03D_MULTI_ENABLE = '多目標偵測模式';
 Blockly.Msg.LJJ_RADAR_03D_MULTI_ENABLE_DATATYPE = [["啟用","0x90"],["關閉","0x80"]];
-Blockly.Msg.LJJ_RADAR_03E_ACTION_LIST = [["快速移動","1"],["緩慢移動","2"],["無生命","0"]];
+Blockly.Msg.LJJ_RADAR_03E_ACTION_LIST = [["快速移動","1"],["靜止或緩慢移動","2"],["無人","0"]];
 
 //PMS7003M
 Blockly.Msg.LJJ_PMS_HELPURL = 'https://sites.google.com/jes.mlc.edu.tw/ljj/linkit7697/PMS7003';
